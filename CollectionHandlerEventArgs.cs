df224039881ac44eb9b3c311487e02da5768cb9d
Sofa Vlasova <147415088+Vlasovasona@users.noreply.github.com>
2024-05-26 16:08:00 +0500

    Update CollectionHandlerEventArgs.cs

--- CollectionHandlerEventArgs.cs
+++ CollectionHandlerEventArgs.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +10,15 @@ namespace Lab_13
 {
     public class CollectionHandlerEventArgs<T>: EventArgs where T : Library_10.IInit, System.ICloneable, new() //класс для передачи информации о событиях
     {
-        public string TypeOfChanges { get; set; } //тип изменений в коллекции
-        public PointHash<T> Element { get; set; } //св-ство для ссылки на объект, скоторым связаны изменения
+        public string TypeOfChanges { get; set; } //тип изменений
+        public PointHash<T> Element { get; set; } //для элемента с которым произошли изменения
+        public PointHash<T> NewElement { get; set; } // для нового элемента (в случае замены)
 
-        public CollectionHandlerEventArgs(string type, PointHash<T> element) //упаковка информации для дальнейшей передачи
+        public CollectionHandlerEventArgs(string type, PointHash<T> element, PointHash<T> newElement) // Include newElement in the constructor
         {
             TypeOfChanges = type;
             Element = element;
+            NewElement = newElement;
         }
 
     }
